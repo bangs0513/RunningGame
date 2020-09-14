06ba6ea45c87124bab5041a5a9a96a4d90b3a8cd
bangs <bsy_0513@naver.com>
2020-09-14 20:37:48 +0900

    DemoVer. 1.0
    V.1.0

---
+++ Assets/Script/etc/AnimTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class AnimTrigger : MonoBehaviour
+{
+    [SerializeField, Header("애니메이션을 활성화 시킬 오브젝트")]
+    private GameObject act;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 플레이어가 트리거를 밟을 시
+        if (other.CompareTag("Player"))
+        {
+            print("트리거");
+            act.GetComponent<Animator>().enabled = true;
+        }
+    }
+}
