18bfb414ecbfbc15c85870cb81b7b230e21b9310
MinhwanKwak <rhkralsghks7@naver.com>
2020-09-29 12:37:33 +0900

    Revert "414"
    This reverts commit 1310db5cb7fd32c6ac98730d2eec82b9d0d7f7e6, reversing
    changes made to 2f43e3e499110b7a115092570f9baaf47abb1143.

---
+++ Assets/Script/Manager/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public CameraManager cameraManager;
+    public Player player;
+    public UiController uiController;
+    
+    public int levelToUnlock = 2;
+    
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+       
+        //씬이 변경되도 파괴하지 않는 기능 
+        GameObject.DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update()
+    {
+       // GameOver();
+    }
+
+    public void GameOver()
+    {
+        if(uiController.mailCount <= 0)
+        {
+            player.playerStatus = Player.PlayerStatus.DEAD;
+        }
+    }
+
+
+
+    public void WinLevel(bool mission)
+    {
+
+        if (mission)
+        {
+            PlayerPrefs.SetInt("levelReached", levelToUnlock);
+            SceneManager.LoadScene("SelectScene");
+        }
+        else
+        {
+            Debug.Log("미션실패");
+        }
+    }
+}
